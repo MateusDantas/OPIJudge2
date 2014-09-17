fe8d4103a7b112b05c6b8b0f65c332f00c308ef7
GustavoMA <gugamalves@yahoo.com.br>
2014-09-17 11:23:53 -0300

    [CONTROLLER] ClarificationController started and ClarificationValidade add;

--- api/api/src/main/java/com/opijudge/controller/ClarificationController.java
+++ api/api/src/main/java/com/opijudge/controller/ClarificationController.java
@@ -2,6 +2,7 @@ package com.opijudge.controller;
 
 import java.util.List;
 
+import com.opijudge.controller.validate.ClarificationValidate;
 import com.opijudge.models.Clarification;
 
 import static com.opijudge.controller.util.Constants.*;
@@ -11,24 +12,40 @@ public class ClarificationController {
 	/**
 	 * This method is responsible for creating and saving to the database
 	 * a new clarification created by some user.
-	 * @param userId Id of the user who created this clarification
 	 * @param problemId Id of the problem in which this clarification came from
+	 * @param userId Id of the user who created this clarification
 	 * @param questionMessage Question asked by the user
 	 * @param type Type of clarification (0 - Private, 1 - Global)
 	 * @return Returns a constant from com.opijudge.controller.util.Constants indicating what happened when
 	 * the clarification was created.
 	 * 
 	 */
-	public int createClarification(int userId, int problemId, String questionMessage, int type) {
-		
-		return 0;
+	public int createClarification(int problemId, int userId, String questionMessage, int type) {
+		try {
+			//TODO still needs to check the problemId and userId - Gustavo
+			if (!ClarificationValidate.isClarificationValid(questionMessage, type)) {
+				return INVALID_MESSAGE;
+			}
+			
+			Clarification clarification = new Clarification(problemId, userId, type, questionMessage);
+			
+			if (!clarification.saveToDatabase()) {
+				return INTERNAL_ERROR;
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			return INTERNAL_ERROR;
+		}
+		return OK;
 	}
 	
 	/**
 	 * This method is responsible for answering a clarification
 	 * @param clarificationId
 	 * @param answerMessage
-	 * @return
+	 * @return Returns a constant from com.opijudge.controller.util.Constants indicating what happened when
+	 * the clarification was created.
 	 */
 	public int answerClarificaton(int clarificationId, String answerMessage) {
 		
